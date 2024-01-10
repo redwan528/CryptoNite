f9049c3074cd154df63eeda2e9ef8752f13b5d86
redwan528 <redwankhan144@gmail.com>
2024-01-10 10:18:04 -0500

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Redwan Khan on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
